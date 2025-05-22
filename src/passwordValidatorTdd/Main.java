5dc0d7892257d4f7c0f4f957d36d995ab52123a2
Otthon de Oliveira Cândido <otthon01candido@gmail.com>
2025-05-21 23:46:12 -0300

    feat: Testing and implementing everything with main

---
+++ src/passwordValidatorTdd/Main.java
@@ -0,0 +1,23 @@
+package passwordValidatorTdd;
+
+public class Main {
+    public static void main(String[] args) {
+        testarSenha("Ab1!");         
+        testarSenha("Abcdefg!");    
+        testarSenha("abcde12!");     
+        testarSenha("Abcde12!");    
+    }
+
+    public static void testarSenha(String senha) {
+        System.out.println("Testando senha: " + senha);
+        PasswordValidatorTdd.ValidationResult resultado = PasswordValidatorTdd.validatePassword(senha);
+
+        if (resultado.isPasswordValid()) {
+            System.out.println("Senha válida ✅\n");
+        } else {
+            System.out.println("Senha inválida ❌");
+            resultado.getErrorMessages().forEach(System.out::println);
+            System.out.println();
+        }
+    }
+}
